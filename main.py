e3020adfd1e81b3a127a13abc886a45c40485f9c
yarynamryhlotska11 <yarynamryhlotska11@gmail.com>
2023-09-26 18:52:19 +0300

    task_5

--- main.py
+++ main.py
@@ -1,29 +1,40 @@
-# Task 1,2
+#Task5
 while True:
     try:
         num1 = float(input("Enter first number: "))
-        num2 = float(input("Enter second number: "))
-        operator = input("Enter operator (+, -, *, /): ")
+    except ValueError:
+        print("Error! Enter a valid number for the first input.")
+        continue  # Ask  again for the first number
 
+    try:
+        num2 = float(input("Enter second number: "))
+    except ValueError:
+        print("Error! Enter a valid number for the second input.")
+        continue   # Ask  again for the second number
+
+    while True:
+        operator = input("Enter operator (+, -, *, /): ")
         if operator in ('+', '-', '*', '/'):
-            break  # Exit the loop if the operator is valid
+            break  # Exit if the operator is correct
         else:
             print("Error! The operator entered is not valid. Please enter one of +, -, *, /.")
-    except ValueError:
-        print("Error! Enter the correct number.")
 
-if operator == '+':
-    result = num1 + num2
-    print(f"The result of the addition: {num1} + {num2} = {result}")
-elif operator == '-':
-    result = num1 - num2
-    print(f"The result of the subtraction: {num1} - {num2} = {result}")
-elif operator == '*':
-    result = num1 * num2
-    print(f"The result of multiplication: {num1} * {num2} = {result}")
-elif operator == '/':
-    if num2 != 0:
-        result = num1 / num2
-        print(f"The result of division: {num1} / {num2} = {result}")
+    if operator == '/' and num2 == 0:
+        print("Error! Division by zero is not allowed.")
     else:
-        print("Error! Division by zero is impossible.")
+        if operator == '+':
+            result = num1 + num2
+            print(f"The result of addition: {num1} + {num2} = {result}")
+        elif operator == '-':
+            result = num1 - num2
+            print(f"The result of subtraction: {num1} - {num2} = {result}")
+        elif operator == '*':
+            result = num1 * num2
+            print(f"The result of multiplication: {num1} * {num2} = {result}")
+        elif operator == '/':
+            result = num1 / num2
+            print(f"The result of division: {num1} / {num2} = {result}")
+
+    repeat = input("Continue to calculate? (Yes/No): ")
+    if repeat.lower() != 'yes':
+        break  # Exit if user don't want to continue
